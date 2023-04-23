e98a08b3558211f48cb9234a06e33181f0ae4006
tlyko <tlyko94@gmail.com>
2023-04-23 18:20:31 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -11,7 +11,7 @@ terraform {
 
 provider "aws" {
   region = var.region
-  profile = "tomek"
+  # profile = "tomek"
   # access_key = var.aws_access_key
   # secret_key = var.aws_secret_key
   # shared_credentials_files = ["/Users/tlyko/.aws/credentials"]

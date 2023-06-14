c12cf7b7aac07bde1a8f796c19ae218dbc499cdd
mackumaciek <maciek.socialmedia@gmail.com>
2023-06-14 18:12:26 +0200

    comment

--- backend.tf
+++ backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "eks-tfstate-dnc"
-    key            = "terraform-aws-eks.tfstate"
-    region         = "eu-central-1"
-    encrypt        = true
+    bucket  = "eks-tfstate-dnc"
+    key     = "terraform-aws-eks.tfstate"
+    region  = "eu-central-1"
+    encrypt = true
   }
 }
+#
